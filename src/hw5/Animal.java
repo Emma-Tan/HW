74b50769ded576e65bce6b4a18f0f2a227a71b9d
Emma Tan <tanjiaxin0515@outlook.com>
2019-11-01 12:28:13 -0400

    # WARNING: head commit changed in the meantime
    first commit

--- src/hw5/Animal.java
+++ src/hw5/Animal.java
@@ -2,11 +2,7 @@ package hw5;
 
 public abstract class Animal implements Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-	
 	
 	String name;
 	int age;
